aa13e3b721460d9c492e9dba33d8cf883f53851e
Jonathan Lu <jonathan.lu31@gmail.com>
2020-12-29 20:12:32 -0800

    Added ability to pencil numbers, added some harder boards, added a timer

--- sudoku_GUI.py
+++ sudoku_GUI.py
@@ -7,17 +7,18 @@ pg.init()
 win = pg.display.set_mode((450, 500))
 pg.display.set_caption('Sudoku Solver')
 enterFont = pg.font.Font('SourceSansPro-Regular.ttf', 30)
+pencilFont = pg.font.Font('SourceSansPro-Regular.ttf', 15)
 
 class Box():
-    def __init__(self, rect, value, pos):
+    def __init__(self, rect, value, pos, fixed):
         self.rect = pg.Rect(rect)
         self.value = value
         self.pencil = 0
         self.highlighted = False
-        self.visited = False
         self.correct = False
         self.wrong = False
         self.pos = pos
+        self.fixed = fixed
 
     def draw(self):
         if self.highlighted:
@@ -30,6 +31,10 @@ class Box():
             number_surface = enterFont.render(str(self.value), True, (0, 0, 0))
             number_rect = number_surface.get_rect(center=(self.rect.centerx, self.rect.centery))
             win.blit(number_surface, number_rect)
+        if self.pencil != 0:
+            pencil_surface = pencilFont.render(str(self.pencil), True, (0, 0, 0, 0.7))
+            pencil_rect = pencil_surface.get_rect(topleft=(self.rect.x + 7, self.rect.y + 3))
+            win.blit(pencil_surface, pencil_rect)
 
 class Grid():
     def __init__(self, grid):
@@ -50,7 +55,10 @@ class Grid():
         formatted = [[] for __ in range(len(self.grid))]
         for y, row in enumerate(self.grid):
             for x, val in enumerate(row):
-                formatted[y].append(Box((50 * x, y * 50, 50, 50), val, (y, x)))
+                if val != 0:
+                    formatted[y].append(Box((50 * x, y * 50, 50, 50), val, (y, x), True))
+                else:
+                    formatted[y].append(Box((50 * x, y * 50, 50, 50), val, (y, x), False))
         return formatted
 
     def update_formatted(self, val, pos):
@@ -69,7 +77,7 @@ class Grid():
                 self.update_formatted(i, empty)
                 self.formatted[y][x].correct = True
                 screenUpdate()
-                # pg.time.delay(10)
+                pg.time.delay(50)
                 
                 if self.solve_GUI():
                     self.formatted[y][x].wrong = False
@@ -85,12 +93,20 @@ class Grid():
 
         return False
 
+def drawTimer():
+    sec = pg.time.get_ticks() // 1000
+    minutes = (sec // 60 if sec >= 60 else 0)
+    time_surface = enterFont.render(f'{minutes:02}:{sec - minutes * 60:02}', True, (0, 0, 0))
+    time_rect = time_surface.get_rect(topleft=(360, 455))
+    win.blit(time_surface, time_rect)
+
 def screenUpdate():
     win.fill((255, 255, 255))
     for row in board.formatted:
         for box in row:
             box.draw()
     board.draw()
+    drawTimer()
     pg.display.update()
 
 run = True
@@ -115,25 +131,27 @@ while run:
     if keys[pg.K_SPACE]:
         board.solve_GUI()
     nums = [keys[pg.K_1], keys[pg.K_2], keys[pg.K_3], keys[pg.K_4], keys[pg.K_5], keys[pg.K_6], keys[pg.K_7], keys[pg.K_8], keys[pg.K_9]]
-    if any(nums):
-        selected.pencil = nums.index(True) + 1
-    if keys[pg.K_RETURN]:
-        selected.value = selected.pencil
-        y, x = selected.pos
-        board.player_grid[y][x] = selected.value
-        if not sudoku.valid(board.player_grid, selected.value, selected.pos):
-            selected.wrong = True
-        else:
-            selected.wrong = False
-
-    if keys[pg.K_p]:
-        pencil_mode = not pencil_mode
-    if keys[pg.K_BACKSPACE]:
-        if pencil_mode:
+    if selected and not selected.fixed:
+        if any(nums):
+            selected.pencil = nums.index(True) + 1
+        if keys[pg.K_RETURN] and selected.pencil:
+            selected.value = selected.pencil
             selected.pencil = 0
-        else:
-            selected.value = 0
+            y, x = selected.pos
+            board.player_grid[y][x] = selected.value
+            if not sudoku.valid(board.player_grid, selected.value, selected.pos):
+                selected.wrong = True
+            else:
+                selected.wrong = False
 
+        if keys[pg.K_p]:
+            pencil_mode = not pencil_mode
+        if keys[pg.K_BACKSPACE]:
+            if pencil_mode:
+                selected.pencil = 0
+            else:
+                selected.value = 0
+    
     screenUpdate()
 
 pg.quit()
